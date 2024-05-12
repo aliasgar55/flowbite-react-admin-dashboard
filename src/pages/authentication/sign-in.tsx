6a079099244fa691e6aea3a09083a69c74d4c6c4
HappyLuckYesterday <milesdanieldragon@gmail.com>
2024-05-12 15:25:21 -0600

    sign up

--- src/pages/authentication/sign-in.tsx
+++ src/pages/authentication/sign-in.tsx
@@ -1,74 +1,45 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+import { Button, Card, Carousel, Checkbox, Label, TextInput } from "flowbite-react";
 import type { FC } from "react";
+import NavbarSidebarLayout from "../../layouts/navbar-sidebar";
 
 const SignInPage: FC = function () {
   return (
-    <div className="flex flex-col items-center justify-center px-6 lg:h-screen lg:gap-y-12">
-      <div className="my-6 flex items-center gap-x-1 lg:my-0">
-        <img
-          alt="Flowbite logo"
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-12"
-        />
-        <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-          Flowbite
-        </span>
+    <NavbarSidebarLayout isFooter={true}>
+      <div className="flex h-screen">
+        <div className="flex-1 flex justify-center items-center" >
+         {/* <div className="flex justify-center"> */}
+            <form className="flex max-w-md w-full flex-col gap-4">
+              <p>WELCOME TO</p>
+              <h1>LUXURY HOTELS</h1>
+              <p>Please create an account </p>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email1" value="Your email" />
+                </div>
+                <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="password1" value="Your password" />
+                </div>
+                <TextInput id="password1" type="password" required />
+              </div>
+              <Button type="submit">Login</Button>
+              <div className="flex items-center gap-2">
+                <Checkbox id="remember" />
+                <Label htmlFor="remember">Forgot password</Label>
+              </div>
+              <Button type="submit" outline>Register</Button>
+            </form>
+          {/* </div> */}
+        </div>
+        <div className="flex-1 bg-gray-400">
+          <img src="/static/image.png" alt="Image description" className="w-full h-auto mx-auto" />
+        </div>
       </div>
-      <Card
-        horizontal
-        imgSrc="/images/authentication/login.jpg"
-        imgAlt=""
-        className="w-full md:max-w-screen-sm [&>img]:hidden md:[&>img]:w-96 md:[&>img]:p-0 md:[&>*]:w-full md:[&>*]:p-16 lg:[&>img]:block"
-      >
-        <h1 className="mb-3 text-2xl font-bold dark:text-white md:text-3xl">
-          Sign in to platform
-        </h1>
-        <form>
-          <div className="mb-4 flex flex-col gap-y-3">
-            <Label htmlFor="email">Your email</Label>
-            <TextInput
-              id="email"
-              name="email"
-              placeholder="name@company.com"
-              type="email"
-            />
-          </div>
-          <div className="mb-6 flex flex-col gap-y-3">
-            <Label htmlFor="password">Your password</Label>
-            <TextInput
-              id="password"
-              name="password"
-              placeholder="••••••••"
-              type="password"
-            />
-          </div>
-          <div className="mb-6 flex items-center justify-between">
-            <div className="flex items-center gap-x-3">
-              <Checkbox id="rememberMe" name="rememberMe" />
-              <Label htmlFor="rememberMe">Remember me</Label>
-            </div>
-            <a
-              href="#"
-              className="w-1/2 text-right text-sm text-primary-600 dark:text-primary-300"
-            >
-              Lost Password?
-            </a>
-          </div>
-          <div className="mb-6">
-            <Button type="submit" className="w-full lg:w-auto">
-              Login to your account
-            </Button>
-          </div>
-          <p className="text-sm text-gray-500 dark:text-gray-300">
-            Not registered?&nbsp;
-            <a href="#" className="text-primary-600 dark:text-primary-300">
-              Create account
-            </a>
-          </p>
-        </form>
-      </Card>
-    </div>
+    </NavbarSidebarLayout>
+
   );
 };
 
--- src/pages/authentication/sign-up.tsx
+++ src/pages/authentication/sign-up.tsx
@@ -1,81 +1,46 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+import { Button, Card, Carousel, Checkbox, Label, TextInput } from "flowbite-react";
 import type { FC } from "react";
+import NavbarSidebarLayout from "../../layouts/navbar-sidebar";
 
-const SignUpPage: FC = function () {
+const SignInPage: FC = function () {
   return (
-    <div className="flex flex-col items-center justify-center px-6 lg:h-screen lg:gap-y-12">
-      <div className="my-6 flex items-center gap-x-1 lg:my-0">
-        <img
-          alt="Flowbite logo"
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-12"
-        />
-        <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-          Flowbite
-        </span>
+    <NavbarSidebarLayout isFooter={true}>
+      <div className="flex h-screen">
+        <div className="flex-1 flex justify-center items-center" >
+         {/* <div className="flex justify-center"> */}
+            <form className="flex max-w-md w-full flex-col gap-4">
+              <p>WELCOME TO</p>
+              <h1>LUXURY HOTELS</h1>
+              <p>Please create an account </p>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email1" value="Your email" />
+                </div>
+                <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="password1" value="Your password" />
+                </div>
+                <TextInput id="password1" type="password" required />
+              </div>
+              <Button type="submit">Login</Button>
+              <div className="flex items-center gap-2">
+                <Checkbox id="remember" />
+                <Label htmlFor="remember">Forgot password</Label>
+              </div>
+              <Button type="submit" outline>Register</Button>
+            </form>
+          {/* </div> */}
+        </div>
+        <div className="flex-1 bg-gray-400">
+          <img src="/static/image.png" alt="Image description" className="w-full h-auto mx-auto" />
+        </div>
       </div>
-      <Card
-        horizontal
-        imgSrc="/images/authentication/create-account.jpg"
-        imgAlt=""
-        className="w-full md:max-w-screen-sm [&>img]:hidden md:[&>img]:w-96 md:[&>img]:p-0 md:[&>*]:w-full md:[&>*]:p-16 lg:[&>img]:block"
-      >
-        <h1 className="mb-3 text-2xl font-bold dark:text-white md:text-3xl">
-          Create a Free Account
-        </h1>
-        <form>
-          <div className="mb-4 flex flex-col gap-y-3">
-            <Label htmlFor="email">Your email</Label>
-            <TextInput
-              id="email"
-              name="email"
-              placeholder="name@company.com"
-              type="email"
-            />
-          </div>
-          <div className="mb-6 flex flex-col gap-y-3">
-            <Label htmlFor="password">Your password</Label>
-            <TextInput
-              id="password"
-              name="password"
-              placeholder="••••••••"
-              type="password"
-            />
-          </div>
-          <div className="mb-6 flex flex-col gap-y-3">
-            <Label htmlFor="confirmPassword">Confirm password</Label>
-            <TextInput
-              id="confirmPassword"
-              name="confirmPassword"
-              placeholder="••••••••"
-              type="password"
-            />
-          </div>
-          <div className="mb-6 flex items-center gap-x-3">
-            <Checkbox id="acceptTerms" name="acceptTerms" />
-            <Label htmlFor="acceptTerms">
-              I accept the&nbsp;
-              <a href="#" className="text-primary-700 dark:text-primary-200">
-                Terms and Conditions
-              </a>
-            </Label>
-          </div>
-          <div className="mb-7">
-            <Button type="submit" className="w-full lg:w-auto">
-              Create account
-            </Button>
-          </div>
-          <p className="text-sm text-gray-500 dark:text-gray-300">
-            Already have an account?&nbsp;
-            <a href="#" className="text-primary-600 dark:text-primary-200">
-              Login here
-            </a>
-          </p>
-        </form>
-      </Card>
-    </div>
+    </NavbarSidebarLayout>
+
   );
 };
 
-export default SignUpPage;
+export default SignInPage;
